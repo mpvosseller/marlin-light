cd2885f91357951a30556f8a4a2e3f78850f0f82
Michael Vosseller <michael.vosseller@gmail.com>
2017-05-23 14:59:49 -0400

    create color palette and assign a random background color from the palette when pressing the settings button.

--- marlin-light/marlin-light/ViewController.swift
+++ marlin-light/marlin-light/ViewController.swift
@@ -9,7 +9,7 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +20,37 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    
+    lazy var availableColors : [UIColor] = {
+        
+        let totalColors = 16
+        var colors : [UIColor] = []
+        
+        // hardcoded colors
+        colors.append(UIColor.white)
+        
+        // auto generated colors
+        
+        // hue 0-360
+        let numToGenerate = totalColors - colors.count
+        let hueStep = 360.0 / Double(numToGenerate)
+        var hue = 0.0
+        
+        while (colors.count < totalColors) {
+            let color = UIColor(hue:CGFloat(hue/360.0), saturation:60.0/100.0, brightness:85.0/100.0, alpha:1.0)
+            colors.append(color)
+            hue += hueStep
+        }
+        
+        return colors
+    }()
+    
+    @IBAction func handleSettingsButtonPressed(_ sender: Any) {
+        let randInt = Int(arc4random())
+        let randIndex = randInt % self.availableColors.count
+        let randColor = self.availableColors[randIndex]
+        self.view.backgroundColor = randColor
+    }
 
 }
 
