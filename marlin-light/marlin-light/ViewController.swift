016cc45527a93eb493bfaa037e6a4a62c0e67171
Michael Vosseller <michael.vosseller@gmail.com>
2017-05-22 20:54:31 -0400

    xcode skeleton project files

---
+++ marlin-light/marlin-light/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  marlin-light
+//
+//  Created by Michael Vosseller on 5/22/17.
+//  Copyright © 2017 MPV Software, LLC. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
