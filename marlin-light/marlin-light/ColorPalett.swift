a57035d6f8c43903dccefa55ece4349035608317
Michael Vosseller <michael.vosseller@gmail.com>
2017-05-29 15:11:30 -0400

    made some magic numbers constants

--- marlin-light/marlin-light/ColorPalett.swift
+++ marlin-light/marlin-light/ColorPalett.swift
@@ -11,30 +11,30 @@ import UIKit
 
 class ColorPalett {
     
-    // hue is an int between 0-360
-    // saturation is an int between 0-100
-    // brightness is an int between 0-100
+    let defaultHue = 192
+    let defaultSaturation = 60
+    let defaultBrightness =  85
+    
+    let maxHue = 360
+    let maxSaturation = 100
+    let maxBrightness = 100
     
     let numColors = 16
     let hue : [Int]
     let saturation : [Int]
-    
-    let defaultHue = 192
-    let defaultSaturation = 60
-    let defaultBrightness =  85
     
     init() {
         
         var hue = [Int]()
         var saturation = [Int]()
         
-        // white
+        // add white
         hue.append(0)
         saturation.append(0)
         
-        // autogenerate the rest of the colors
+        // autogenerate and add the rest of the colors
         let numColorsToAutoGenerate = numColors - hue.count
-        let hueStep = 360 / Double(numColorsToAutoGenerate)
+        let hueStep = Double(maxHue) / Double(numColorsToAutoGenerate)
         
         for index in 0..<numColorsToAutoGenerate {
             let h = Int(hueStep * Double(index))
@@ -47,7 +47,7 @@ class ColorPalett {
     }
     
     func saturationAtIndex(_ index:Int)  -> Int {
-       return self.saturation[index]
+        return self.saturation[index]
     }
     
     func hueAtIndex(_ index:Int) -> Int {
@@ -58,15 +58,14 @@ class ColorPalett {
         let hue = hueAtIndex(index)
         let saturation = saturationAtIndex(index)
         return colorWithHue(hue, saturation:saturation, brightness: brightness)
-
     }
     
     func colorWithHue(_ hue:Int, saturation:Int, brightness:Int) -> UIColor {
-        let hueFloat = CGFloat(hue) / 360.0
-        let saturationFloat = CGFloat(saturation) / 100.0
-        let brightnessFloat = CGFloat(brightness) / 100.0
+        let hueFloat = CGFloat(hue) / CGFloat(maxHue)
+        let saturationFloat = CGFloat(saturation) / CGFloat(maxSaturation)
+        let brightnessFloat = CGFloat(brightness) / CGFloat(maxBrightness)
         let alphaFloat = CGFloat(1.0)
         return UIColor(hue:hueFloat, saturation:saturationFloat, brightness:brightnessFloat, alpha:alphaFloat)
     }
-
+    
 }
