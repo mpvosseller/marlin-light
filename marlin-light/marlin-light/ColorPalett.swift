0b7b8a3418d083adb415cf2dd925a6a8d273cc4a
Michael Vosseller <michael.vosseller@gmail.com>
2017-05-30 11:49:43 -0400

    code cleanup

--- marlin-light/marlin-light/ColorPalett.swift
+++ marlin-light/marlin-light/ColorPalett.swift
@@ -36,8 +36,7 @@ class ColorPalett {
         let hueStep = Double(ColorPalett.maxHue) / Double(numColorsToAutoGenerate)
         
         for index in 0..<numColorsToAutoGenerate {
-            let h = Int(hueStep * Double(index))
-            hue.append(h)
+            hue.append(Int(hueStep * Double(index)))
             saturation.append(ColorPalett.defaultSaturation)
         }
         
@@ -59,6 +58,10 @@ class ColorPalett {
         return colorWithHue(hue, saturation:saturation, brightness: brightness)
     }
     
+    func defaultColor() -> UIColor {
+        return colorWithHue(ColorPalett.defaultHue, saturation:ColorPalett.defaultSaturation, brightness:ColorPalett.defaultBrightness)
+    }
+
     func colorWithHue(_ hue:Int, saturation:Int, brightness:Int) -> UIColor {
         let hueFloat = CGFloat(hue) / CGFloat(ColorPalett.maxHue)
         let saturationFloat = CGFloat(saturation) / CGFloat(ColorPalett.maxSaturation)
@@ -66,9 +69,4 @@ class ColorPalett {
         let alphaFloat = CGFloat(1.0)
         return UIColor(hue:hueFloat, saturation:saturationFloat, brightness:brightnessFloat, alpha:alphaFloat)
     }
-    
-    func defaultColor() -> UIColor {
-        return colorWithHue(ColorPalett.defaultHue, saturation:ColorPalett.defaultSaturation, brightness:ColorPalett.defaultBrightness)
-    }
-    
 }
